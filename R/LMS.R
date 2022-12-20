8de2e1e7e428ef9a7dd1e5bccbe658c4627348e1
Julien Patrick Irmer <julienirmer_17@yahoo.de>
2022-12-20 18:15:59 +0100

    Add data transformations to handle manifest variables to LMS in Mplus and add parameter table merge.

--- R/LMS.R
+++ R/LMS.R
@@ -7,22 +7,39 @@
 #' @export
 
 
-
-
-LMS <- function(lavModel_Analysis, data, prefix = 1, algorithm = "INTEGRATION")
+LMS <- function(lavModel_Analysis, data, data_transformations = NULL, prefix = 1, algorithm = "INTEGRATION")
 {
      if(!dir.exists("temp")) dir.create("temp/")
 
      input_file <- paste0("temp/", prefix, "_runScript_LMS.inp")
      output_file <- paste0("temp/", prefix, "_runScript_LMS.out")
-     data_file <- paste0("temp/", prefix, "data_temp.dat")
+     data_file <- paste0("temp/", prefix, "_data_temp.dat")
+     lavModel_Analysis_LMS <- lavModel_Analysis
 
+     # remove non-fittable coefficients
+     lavModel_Analysis_LMS <- lavModel_Analysis_LMS[!((grepl(":", lavModel_Analysis_LMS$lhs) &
+                                                            (lavModel_Analysis_LMS$op %in% c("~~", "~1"))) |
+                                                     (grepl(":", lavModel_Analysis_LMS$rhs) &
+                                                           (lavModel_Analysis_LMS$op %in% c("~~", "~1")))),, drop = F]
 
-     write.table(x = data, file = data_file, col.names = F, row.names = F)
-     varnames <- names(data)
+     # transform data
+     if(!is.null(data_transformations))
+     {
+          NL_data <- sapply(1:nrow(data_transformations), FUN = function(d){v1 <- data[, data_transformations$V1[d]]
+          v2 <- data[, data_transformations$V2[d]]
+          return(scale(v1, center = T, scale = F)*scale(v2, center = T, scale = F))
+          })
+          NL_data <- data.frame(NL_data); names(NL_data) <- data_transformations$newname
+          data_transformed <- cbind(data, NL_data)
+     }else{
+          data_transformed <- data
+     }
+
+     write.table(x = data_transformed, file = data_file, col.names = F, row.names = F)
+     varnames <- names(data_transformed)
 
      # measurement model
-     temp_measurement <- lavModel_Analysis[lavModel_Analysis$op == "=~", c("lhs", "rhs", "start", "fixed"), drop = F]
+     temp_measurement <- lavModel_Analysis_LMS[lavModel_Analysis_LMS$op == "=~", c("lhs", "rhs", "start", "fixed"), drop = F]
      if(nrow(temp_measurement)>0)
      {
           Measurement_models <- apply(temp_measurement, 1,
@@ -35,7 +52,7 @@ LMS <- function(lavModel_Analysis, data, prefix = 1, algorithm = "INTEGRATION")
 
 
      # nonlinear latent effects
-     temp_NL_lv <- lavModel_Analysis[lavModel_Analysis$LHSvarType == "funExo", c("lhs")]
+     temp_NL_lv <- lavModel_Analysis_LMS[lavModel_Analysis_LMS$RHSvarType == "funExo", c("rhs")]
      if(length(temp_NL_lv) > 0)
      {
           NL_lv_effects <- unique(temp_NL_lv)
@@ -51,25 +68,25 @@ LMS <- function(lavModel_Analysis, data, prefix = 1, algorithm = "INTEGRATION")
      }
 
 
-     lavModel_Analysis_temp <- lavModel_Analysis
+     lavModel_Analysis_LMS_temp <- lavModel_Analysis_LMS
      to_replace <- NL_lv_effects; replacement <- NL_lv_effects_names
      if(length(NL_lv_effects) > 0)
      {
           for(i in seq_along(NL_lv_effects))
           {
-               lavModel_Analysis_temp$lhs[lavModel_Analysis_temp$lhs == to_replace[i]] <- replacement[i]
-               lavModel_Analysis_temp$rhs[lavModel_Analysis_temp$rhs == to_replace[i]] <- replacement[i]
+               lavModel_Analysis_LMS_temp$lhs[lavModel_Analysis_LMS_temp$lhs == to_replace[i]] <- replacement[i]
+               lavModel_Analysis_LMS_temp$rhs[lavModel_Analysis_LMS_temp$rhs == to_replace[i]] <- replacement[i]
           }
      }
 
-     Structural_model <- apply(lavModel_Analysis_temp[lavModel_Analysis_temp$op == "~", c("lhs", "rhs", "start", "fixed"),
+     Structural_model <- apply(lavModel_Analysis_LMS_temp[lavModel_Analysis_LMS_temp$op == "~", c("lhs", "rhs", "start", "fixed"),
                                                       drop = F], 1,
                                FUN = function(x) paste0(x[1], " ON ", x[2],
                                                         ifelse(test = x[4], yes = "@", no = "*"),
                                                         round(as.numeric(x[3]), 3), ";"))
 
      # variances and residual variances
-     Variances <- apply(lavModel_Analysis_temp[lavModel_Analysis_temp$rhs == lavModel_Analysis_temp$lhs & lavModel_Analysis_temp$op == "~~",
+     Variances <- apply(lavModel_Analysis_LMS_temp[lavModel_Analysis_LMS_temp$rhs == lavModel_Analysis_LMS_temp$lhs & lavModel_Analysis_LMS_temp$op == "~~",
                                                c("lhs", "rhs", "start", "fixed", "LHSvarType"), drop = F], 1,
                         FUN = function(x){ out <- paste0(x[1],
                                                          ifelse(test = x[4], yes = "@", no = "*"),
@@ -78,7 +95,7 @@ LMS <- function(lavModel_Analysis, data, prefix = 1, algorithm = "INTEGRATION")
                         out})
      Variances <- Variances[Variances != ""]
 
-     temp <- lavModel_Analysis_temp[lavModel_Analysis_temp$rhs != lavModel_Analysis_temp$lhs & lavModel_Analysis_temp$op == "~~",
+     temp <- lavModel_Analysis_LMS_temp[lavModel_Analysis_LMS_temp$rhs != lavModel_Analysis_LMS_temp$lhs & lavModel_Analysis_LMS_temp$op == "~~",
                                     c("lhs", "rhs", "start", "fixed", "LHSvarType", "RHSvarType"), drop = F]
      if(nrow(temp) > 0)
      {
@@ -89,7 +106,8 @@ LMS <- function(lavModel_Analysis, data, prefix = 1, algorithm = "INTEGRATION")
                Covariances <- apply(temp, 1,
                                     FUN = function(x){ out <- paste0(x[1], " WITH ", x[2],
                                                                      ifelse(test = x[4], yes = "@", no = "*"),
-                                                                     round(as.numeric(x[3]), 3), ";")
+                                                                     ifelse(test = is.na(round(as.numeric(x[3]), 3)),
+                                                                            yes = "", no =  round(as.numeric(x[3]), 3)), ";")
                                     out})
           }else{
                Covariances <- ""
@@ -99,6 +117,18 @@ LMS <- function(lavModel_Analysis, data, prefix = 1, algorithm = "INTEGRATION")
      }
 
 
+     # Means and intercepts
+     temp_means <- lavModel_Analysis_LMS[lavModel_Analysis_LMS$op == "~1", c("lhs", "rhs", "start", "fixed"), drop = F]
+     if(nrow(temp_means)>0)
+     {
+          Mean_models <- apply(temp_means, 1,
+                                      FUN = function(x) paste0("[", x[1],
+                                                               ifelse(test = x[4], yes = "@", no = "*"),
+                                                               ifelse(test = is.na(round(as.numeric(x[3]), 3)),
+                                                                      yes = "", no =  round(as.numeric(x[3]), 3)), "];"))
+     }else{
+          Measurement_models <- ""
+     }
 
 
      ### nonlinear effects of ovs missing for now
@@ -108,7 +138,7 @@ TITLE:
      temp', prefix, '
 
 DATA:
-     FILE = ', prefix,'data_temp.dat;
+     FILE = ', prefix,'_data_temp.dat;
 
 VARIABLE:
      NAMES =\n             ', paste(varnames, sep = "", collapse = "\n             "),';
@@ -128,15 +158,49 @@ MODEL:
                          '\n\n     !Variances
              ', paste(Variances, sep = "", collapse ="\n             "),
                          '\n\n     !Covariances
-             ', paste(Covariances, sep = "", collapse ="\n             "))
+             ', paste(Covariances, sep = "", collapse ="\n             "),
+                         '\n\n     !Means and Intercepts
+             ', paste(Mean_models, sep = "", collapse ="\n             "))
 
      cat(LMS_input, file = input_file, append = F)
      MplusAutomation::runModels(target = input_file)
      fitMplus <- MplusAutomation::readModels(target = output_file)
 
-     fitMplus$parameters$unstandardized
+     Parameters <- data.frame(fitMplus$parameters$unstandardized)
+
+     # check convergence
+     if(is.null(Parameters$se))
+     {
+          Parameters$est <- NA; Parameters$se <- NA; Parameters$est_se <- NA; Parameters$pval <- NA
+     }else{
+          if(any(Parameters$se > 20*max(as.numeric(lavModel_Analysis_LMS$start), na.rm = T)))
+          {
+               Parameters$est <- NA; Parameters$se <- NA; Parameters$est_se <- NA; Parameters$pval <- NA
+          }
+     }
+
+     header <- Parameters$paramHeader
+     param <- Parameters$param
+     Label <- header
+     Label <- stringr::str_replace(Label, pattern = ".BY", replacement = "=~")
+     Label <- stringr::str_replace(Label, pattern = ".ON", replacement = "~")
+     Label <- stringr::str_replace(Label, pattern = ".WITH", replacement = "~~")
+     Label[header == "Residual.Variances"] <- paste0(param[header == "Residual.Variances"], "~~")
+     Label[header == "Variances"] <- paste0(param[header == "Variances"], "~~")
+     Label[header == "Intercepts" | header == "Means"] <- param[header == "Intercepts" | header == "Means"]
+     param[header == "Intercepts" | header == "Means"] <- "~1"
+     Parameters$matchLabel <- toupper(paste0(Label, param))
+     Parameters$matchLabel <- stringr::str_replace_all(string = Parameters$matchLabel, pattern = "_", replacement = ":")
+
+     lavModel_Analysis_LMS$matchLabel <- toupper(paste0(lavModel_Analysis_LMS$lhs, lavModel_Analysis_LMS$op, lavModel_Analysis_LMS$rhs))
+
+     lavModel_Analysis_LMS <- merge(x = lavModel_Analysis_LMS, y = Parameters, by = "matchLabel")
+     lavModel_Analysis_LMS <- lavModel_Analysis_LMS[order(lavModel_Analysis_LMS$id),]
+     lavModel_Analysis_LMS$paramHeader <- NULL;      lavModel_Analysis_LMS$param <- NULL;
 
      # delete temp files
      file.remove(data_file); file.remove(input_file); file.remove(output_file)
+
+     return(lavModel_Analysis_LMS)
 }
 
