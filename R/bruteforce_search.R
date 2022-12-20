cd2ebde0191a6aba964f8219fc22b9dd3a13b1b8
Julien Patrick Irmer <julienirmer_17@yahoo.de>
2022-12-20 18:35:10 +0100

    Initialize bruteforce search function.

---
+++ R/bruteforce_search.R
@@ -0,0 +1,11 @@
+
+bruteforce_search <- function(POI,
+                              method, lavModel,
+                              N_start = nrow(lavModel)*10,
+                              Ntotal = 1000,
+                              power_aim = .8, alpha = .05, lb = nrow(lavModel),
+                              ...)
+{
+     dotdotdot <- list(...)
+
+}
