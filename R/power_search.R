699ca8bbd2a3d8dabc9fb08bab87685c42b3c231
Julien Patrick Irmer <julienirmer_17@yahoo.de>
2022-12-20 18:35:56 +0100

    Add documentation to power function and add first working smart search.

--- R/power_search.R
+++ R/power_search.R
@@ -1,23 +1,55 @@
 #' Search function to find N for desired power
-#' @param method Method used to fit to the data.
+#' @param POI Parameter Of Interest as a vector of strings. Must be in lavaan-syntax without any spaces. Nonlinear effects should have the same ordering as in model.
+#' @param method Method used to fit to the data. Can be LMS or UPI.
 #' @param lavModel lavModel object describing the model.
+#' @param search_method String stating the search method. "smart" or "bruteforce".
+#' @param Ntotal Total number of models to be fitted. Higher number results in higher precision and longer runtime.
+#' @param power_aim Minimal power value to approximate. Default to .8.
+#' @param alpha Type I-error rate. Default to .05.
+#' @param ... Additional arguments passed on to the search functions.
 #' @import MplusAutomation
 #' @export
-power_search <- function(method, lavModel,
+#'
+power_search <- function(POI,
+                         method, lavModel,
                          search_method,
-                         N_start = 200, type = "u",
-                         Ntotal = 1000, steps = 10,
-                         power_aim = .8, alpha = .05, ...)
+                         Ntotal = 1000,
+                         power_aim = .8,
+                         alpha = .05, ...)
 {
      dotdotdot <- list(...)
+
+     if(!is.null(dotdotdot$N_start)){
+          N_start <- dotdotdot$N_start
+     }else{
+          N_start <- nrow(lavModel)*10
+     }
+     if(!is.null(dotdotdot$lb)){
+          lb <- max(dotdotdot$lb, nro(lavModel))
+     }else{
+          lb <- nrow(lavModel)
+     }
+     if(!is.null(dotdotdot$steps)){
+          steps <- dotdotdot$steps
+     }else{
+          steps <- 20
+     }
+     if(!is.null(dotdotdot$type)){
+          type <- dotdotdot$type
+     }else{
+          type <- "u"
+     }
+
+     if(tolower(search_method %in% c("smart", "smart_search")))
+     {
+          args <- names(formals(smart_search))
+          out <- do.call("smart_search", args = mget(args))
+     }else if(tolower(search_method %in% c("bruteforce", "brute", "force", "brute force", "brute_force")))
+     {
+          args <- names(formals(bruteforce_search))
+          out <- do.call("bruteforce_search", args = mget(args))
+     }
+
+     return(out)
 }
 
-
-
-test <- function(x, ...)
-{
-     dotdotdot <- list(...)
-     print(dotdotdot)
-     return(x+1)
-}
-test(x = 2, a = 21)
