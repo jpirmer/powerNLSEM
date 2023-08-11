44ad62bde6a9167d0b04e35f3aa4c966be19be52
Julien Patrick Irmer <julienirmer_17@yahoo.de>
2023-08-11 15:30:46 +0200

    Comply with CRAN and add documentation.

--- R/sim_and_fit.R
+++ R/sim_and_fit.R
@@ -1,3 +1,4 @@
+#' @import stats
 
 sim_and_fit <- function(n, POI, method,
                         alpha,
@@ -15,15 +16,15 @@ sim_and_fit <- function(n, POI, method,
           # Mplus only works with upper cases:
           POI <- toupper(POI); lavModel_Analysis$matchLabel <- toupper(lavModel_Analysis$matchLabel)
           fit <- try(LMS(lavModel_Analysis = lavModel_Analysis, data = data,
-                         data_transformations = data_transformations, prefix = prefix), silent = T)
+                         data_transformations = data_transformations, prefix = prefix), silent = TRUE)
      }else if(tolower(method) %in% c("path", "regression", "scaleregression", "sr", "reg")){
           fit <- try(SR(lavModel_Analysis = lavModel_Analysis, data = data,
-                        data_transformations = data_transformations), silent = T)
+                        data_transformations = data_transformations), silent = TRUE)
      }
      if(!inherits(fit, "try-error"))
      {
-          fit <- fit[fit$matchLabel %in% POI,,drop = F]
-          out <- try(abs(fit$est/fit$se), silent = T)
+          fit <- fit[fit$matchLabel %in% POI,,drop = FALSE]
+          out <- try(abs(fit$est/fit$se), silent = TRUE)
           if(!inherits(out, "try-error"))
           {
                out <- out > qnorm(p = 1-alpha/2)
