eb5be37cea3df4b7bdc0fd26786b6d2ad274bbb6
Julien Patrick Irmer <julienirmer_17@yahoo.de>
2023-08-11 15:27:53 +0200

    Comply with CRAN and add documentation.

--- R/UPI.R
+++ R/UPI.R
@@ -2,6 +2,8 @@
 #' @param lavModel_Analysis the lavModel_Analysis object
 #' @param data set to fit
 #' @param data_transformations Data transformations
+#' @import stats
+#' @import utils
 #' @export
 
 
@@ -9,8 +11,6 @@ UPI <- function(lavModel_Analysis, data,
                 data_transformations = NULL)
 {
      lavModel_Analysis_UPI <- lavModel_Analysis
-
-     lavModel_attributes
 
      ## use semTools for product indicators
      ## use MLR
@@ -20,7 +20,7 @@ UPI <- function(lavModel_Analysis, data,
      {
           NL_data <- sapply(1:nrow(data_transformations), FUN = function(d){v1 <- data[, data_transformations$V1[d]]
           v2 <- data[, data_transformations$V2[d]]
-          return(scale(v1, center = T, scale = F)*scale(v2, center = T, scale = F))
+          return(scale(v1, center = TRUE, scale = FALSE)*scale(v2, center = TRUE, scale = FALSE))
           })
           NL_data <- data.frame(NL_data); names(NL_data) <- data_transformations$newname
           data_transformed <- cbind(data, NL_data)
@@ -30,39 +30,39 @@ UPI <- function(lavModel_Analysis, data,
 
 
 
-     Parameters <- data.frame(fitMplus$parameters$unstandardized)
-
-     # check convergence
-     if(is.null(Parameters$se))
-     {
-          Parameters$est <- NA; Parameters$se <- NA; Parameters$est_se <- NA; Parameters$pval <- NA
-     }else{
-          if(any(Parameters$se > 20*max(as.numeric(lavModel_Analysis_LMS$start), na.rm = T)))
-          {
-               Parameters$est <- NA; Parameters$se <- NA; Parameters$est_se <- NA; Parameters$pval <- NA
-          }
-     }
-
-     header <- Parameters$paramHeader
-     param <- Parameters$param
-     Label <- header
-     Label <- stringr::str_replace(Label, pattern = ".BY", replacement = "=~")
-     Label <- stringr::str_replace(Label, pattern = ".ON", replacement = "~")
-     Label <- stringr::str_replace(Label, pattern = ".WITH", replacement = "~~")
-     Label[header == "Residual.Variances"] <- paste0(param[header == "Residual.Variances"], "~~")
-     Label[header == "Variances"] <- paste0(param[header == "Variances"], "~~")
-     Label[header == "Intercepts" | header == "Means"] <- param[header == "Intercepts" | header == "Means"]
-     param[header == "Intercepts" | header == "Means"] <- "~1"
-     Parameters$matchLabel <- toupper(paste0(Label, param))
-     Parameters$matchLabel <- stringr::str_replace_all(string = Parameters$matchLabel, pattern = "_", replacement = ":")
-
-     lavModel_Analysis_LMS <- merge(x = lavModel_Analysis_LMS, y = Parameters, by = "matchLabel")
-     lavModel_Analysis_LMS <- lavModel_Analysis_LMS[order(lavModel_Analysis_LMS$id),]
-     lavModel_Analysis_LMS$paramHeader <- NULL;      lavModel_Analysis_LMS$param <- NULL;
+     # Parameters <- data.frame(fitMplus$parameters$unstandardized)
+#
+#      # check convergence
+#      if(is.null(Parameters$se))
+#      {
+#           Parameters$est <- NA; Parameters$se <- NA; Parameters$est_se <- NA; Parameters$pval <- NA
+#      }else{
+#           if(any(Parameters$se > 20*max(as.numeric(lavModel_Analysis_LMS$start), na.rm = TRUE)))
+#           {
+#                Parameters$est <- NA; Parameters$se <- NA; Parameters$est_se <- NA; Parameters$pval <- NA
+#           }
+#      }
+#
+#      header <- Parameters$paramHeader
+#      param <- Parameters$param
+#      Label <- header
+#      Label <- stringr::str_replace(Label, pattern = ".BY", replacement = "=~")
+#      Label <- stringr::str_replace(Label, pattern = ".ON", replacement = "~")
+#      Label <- stringr::str_replace(Label, pattern = ".WITH", replacement = "~~")
+#      Label[header == "Residual.Variances"] <- paste0(param[header == "Residual.Variances"], "~~")
+#      Label[header == "Variances"] <- paste0(param[header == "Variances"], "~~")
+#      Label[header == "Intercepts" | header == "Means"] <- param[header == "Intercepts" | header == "Means"]
+#      param[header == "Intercepts" | header == "Means"] <- "~1"
+#      Parameters$matchLabel <- toupper(paste0(Label, param))
+#      Parameters$matchLabel <- stringr::str_replace_all(string = Parameters$matchLabel, pattern = "_", replacement = ":")
+#
+#      lavModel_Analysis_LMS <- merge(x = lavModel_Analysis_LMS, y = Parameters, by = "matchLabel")
+#      lavModel_Analysis_LMS <- lavModel_Analysis_LMS[order(lavModel_Analysis_LMS$id),]
+#      lavModel_Analysis_LMS$paramHeader <- NULL;      lavModel_Analysis_LMS$param <- NULL;
 
      # delete temp files
-     file.remove(data_file); file.remove(input_file); file.remove(output_file)
+     # file.remove(data_file); file.remove(input_file); file.remove(output_file)
 
-     return(lavModel_Analysis_LMS)
+     return(lavModel_Analysis_UPI)
 }
 
