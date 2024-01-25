ce4f423bae4fedde5b6749803c35bee5ceb99401
Julien Patrick Irmer <jpirmer@users.noreply.github.com>
2024-01-25 16:00:16 +0100

    Update sim_and_fit to include fitOK and only est and se.

--- R/sim_and_fit.R
+++ R/sim_and_fit.R
@@ -39,31 +39,21 @@ sim_and_fit <- function(n, POI, method,
           fit <- merge(df_POI, fit_temp, by.x = "matchLabel", sort = FALSE)
 
           est <- try(fit$est, silent = TRUE); se <- try(fit$se, silent = TRUE)
-          true <- try(fit$ustart, silent = TRUE)
-          pvalue_onesided <- try(pnorm(sign(true)*est / se, lower.tail = FALSE), silent = TRUE)
-          pvalue_twosided <- try(2*pnorm(abs(est) / se, lower.tail = FALSE), silent = TRUE)
 
-          if(!inherits(pvalue_onesided, "try-error") & !inherits(pvalue_twosided, "try-error"))
+          if(!inherits(est/se, "try-error"))
           {
-               sigs_onesided <- pvalue_onesided < alpha
-               sigs_twosided <- pvalue_twosided < alpha
-
                fitOK <- TRUE #### !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Probably add better check ---------------------------------------------
-
-               out <- list("est" = est, "se" = se, "pvalue_onesided" = pvalue_onesided,
-                           "pvalue_twosided" = pvalue_twosided,
-                           "sigs_onesided" = sigs_onesided, "sigs_twosided" = sigs_twosided)
+               if(any(is.na(se))) fitOK <- FALSE
+               out <- list("est" = est, "se" = se)
 
                }else{
-               out <- lapply(1:6, function(x) return(rep(NA, length(POI))))
-               names(out) <- c("est", "se", "pvalue_onesided", "pvalue_twosided",
-                               "sigs_onesided", "sigs_twosided")
+               out <- lapply(1:2, function(x) return(rep(NA, length(POI))))
+               names(out) <- c("est", "se")
                fitOK <- FALSE
           }
      }else{
-          out <- lapply(1:6, function(x) return(rep(NA, length(POI))))
-          names(out) <- c("est", "se", "pvalue_onesided", "pvalue_twosided",
-                          "sigs_onesided", "sigs_twosided")
+          out <- lapply(1:2, function(x) return(rep(NA, length(POI))))
+          names(out) <- c("est", "se")
           fitOK <- FALSE
      }
      out <- lapply(out, function(x){names(x) <- POI
