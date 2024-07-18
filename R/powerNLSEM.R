4e852eff7d1e6cd1d16eb4f62ab1c5422b882d6a
Julien Irmer <julienirmer_17@yahoo.de>
2024-07-18 17:16:37 +0200

    Add examples for publication on CRAN.

--- R/powerNLSEM.R
+++ R/powerNLSEM.R
@@ -24,6 +24,49 @@
 #' @references Little, T. D., Bovaird, J. A., & Widaman, K. F. (2006). On the merits of orthogonalizing powered and product terms: Implications for modeling interactions among latent variables. _Structural Equation Modeling, 13_(4), 497–519. \doi{10.1207/s15328007sem1304_1}
 #' @references Marsh, H. W., Wen, Z. & Hau, K. T. (2004). Structural equation models of latent interactions: Evaluation of alternative estimation strategies and indicator construction. _Psychological Methods, 9_(3), 275–300. \doi{10.1037/1082-989X.9.3.275}
 #' @references Ng, J. C. K., & Chan, W. (2020). Latent moderation analysis: A factor score approach. _Structural Equation Modeling: A Multidisciplinary Journal, 27_(4), 629–648. \doi{10.1080/10705511.2019.1664304}.
+#' @examples
+#' \dontrun{
+#' # write model in lavaan syntax
+#' model <- "
+#' # measurement models
+#'           X =~ 1*x1 + 0.8*x2 + 0.7*x3
+#'           Y =~ 1*y1 + 0.85*y2 + 0.78*y3
+#'           Z =~ 1*z1 + 0.9*z2 + 0.6*z3
+#'
+#' # structural models
+#'           Y ~ 0.3*X + .2*Z +  .2*X:Z
+#'
+#' # residual variances
+#'          Y~~.7975*Y
+#'          X~~1*X
+#'          Z~~1*Z
+#'
+#' # covariances
+#'          X~~0.5*Z
+#'
+#' # measurement error variances
+#'          x1~~.1*x1
+#'          x2~~.2*x2
+#'          x3~~.3*x3
+#'          z1~~.2*z1
+#'          z2~~.3*z2
+#'          z3~~.4*z3
+#'          y1~~.5*y1
+#'          y2~~.4*y2
+#'          y3~~.3*y3
+#' "
+#' # run model-implied simulation-based power estimation
+#' # for the effects: c("Y~X", "Y~Z", "Y~X:Z")
+#' Result_Power <- powerNLSEM(model = model, POI = c("Y~X", "Y~Z", "Y~X:Z"),
+#'                            method = "UPI", search_method = "adaptive",
+#'                            steps = 10, power_modeling_method = "probit",
+#'                            R = 1000, power_aim = .8, alpha = .05,
+#'                            alpha_power_modeling = .05,
+#'                            CORES = 1, seed = 2024)
+#'
+#' Result_Power
+#' }
+#'
 #' @export
 
 powerNLSEM <- function(model, POI,
--- R/summary.powerNLSEM.R
+++ R/summary.powerNLSEM.R
@@ -5,6 +5,50 @@
 #' @param alpha Type I-error rate for significance decision. Default to \code{NULL} (if \code{NULL}, \code{alpha} of the original MSPE is used).
 #' @param ... Further arguments to use in \code{summary}.
 #' @return summary of powerNLSEM object
+#' @examples
+#' \dontrun{
+#' # write model in lavaan syntax
+#' model <- "
+#' # measurement models
+#'           X =~ 1*x1 + 0.8*x2 + 0.7*x3
+#'           Y =~ 1*y1 + 0.85*y2 + 0.78*y3
+#'           Z =~ 1*z1 + 0.9*z2 + 0.6*z3
+#'
+#' # structural models
+#'           Y ~ 0.3*X + .2*Z +  .2*X:Z
+#'
+#' # residual variances
+#'          Y~~.7975*Y
+#'          X~~1*X
+#'          Z~~1*Z
+#'
+#' # covariances
+#'          X~~0.5*Z
+#'
+#' # measurement error variances
+#'          x1~~.1*x1
+#'          x2~~.2*x2
+#'          x3~~.3*x3
+#'          z1~~.2*z1
+#'          z2~~.3*z2
+#'          z3~~.4*z3
+#'          y1~~.5*y1
+#'          y2~~.4*y2
+#'          y3~~.3*y3
+#' "
+#' # run model-implied simulation-based power estimation
+#' # for the effects: c("Y~X", "Y~Z", "Y~X:Z")
+#' Result_Power <- powerNLSEM(model = model, POI = c("Y~X", "Y~Z", "Y~X:Z"),
+#'                            method = "UPI", search_method = "adaptive",
+#'                            steps = 10, power_modeling_method = "probit",
+#'                            R = 1000, power_aim = .8, alpha = .05,
+#'                            alpha_power_modeling = .05,
+#'                            CORES = 1, seed = 2024)
+#'
+#' Result_Power
+#' summary(Result_Power)
+#' }
+#' @export
 #' @exportS3Method summary powerNLSEM
 #' @export
 
